c575ca7f11fdde632d3cf80453588c740413d46d
gabogabriel <gabogabriel@1ca0b7f4-8ced-11de-a676-0be261666330>
2009-11-23 16:21:44 +0000

    :3

---
+++ Entregas/Entrega 4/SmartMusicFrontEnd/SmartMusic/ColaCircular.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartMusic
+{
+    class ColaCircular
+    {
+        private int puntero;
+        private int suma;
+        private int[] cola;
+
+        public ColaCircular(int largo)
+        {
+            puntero = 0;
+            suma = largo;
+            cola = new int[largo];
+
+            for (int i = 0; i < cola.Length; i++)
+                cola[i] = 1;
+        }
+
+        public int agregar(int x)
+        {
+            suma = suma - cola[puntero] + x;
+            cola[puntero] = x;
+            puntero = (puntero + 1) % cola.Length;
+            return suma / cola.Length;
+        }
+    }
+}
